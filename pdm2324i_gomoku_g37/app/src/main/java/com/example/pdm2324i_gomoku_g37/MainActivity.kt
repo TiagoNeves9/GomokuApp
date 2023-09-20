17696819760100b4c849e9ff07ca97684353cbcf
A48264 - João Pereira <a48264@alunos.isel.pt>
2023-09-20 18:13:19 +0100

    AuthorsDisplay 1.0

--- pdm2324i_gomoku_g37/app/src/main/java/com/example/pdm2324i_gomoku_g37/MainActivity.kt
+++ pdm2324i_gomoku_g37/app/src/main/java/com/example/pdm2324i_gomoku_g37/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pdm2324i_gomoku_g37.ui.theme.Pdm2324i_gomoku_g37Theme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +23,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Group 37")
+                    AuthorsDisplay()
                 }
             }
         }
     }
+}
+
+@Composable
+fun AuthorsDisplay(modifier: Modifier = Modifier) {
+    Text(
+        text = "Authors:\n" +
+                "\t48264\t-\tJoão Pereira\n" +
+                "\t48292\t-\tTiago Neves\n" +
+                "\t48333\t-\tTomás Barroso",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AuthorsDisplayPreview() {
+    AuthorsDisplay()
 }
 
 @Composable
@@ -41,6 +60,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Pdm2324i_gomoku_g37Theme {
-        Greeting("Android")
+        Greeting("Android - Preview")
     }
 }
