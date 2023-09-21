5f31d95f05da118676c723ca6207a99459574313
tomasbarroso2000 <tomasbarroso2000@gmail.com>
2023-09-21 11:58:57 +0100

    start of Authors screen design

--- pdm2324i_gomoku_g37/app/src/main/java/com/example/pdm2324i_gomoku_g37/screens/AuthorsScreen.kt
+++ pdm2324i_gomoku_g37/app/src/main/java/com/example/pdm2324i_gomoku_g37/screens/AuthorsScreen.kt
@@ -1,24 +1,104 @@
 package com.example.pdm2324i_gomoku_g37.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pdm2324i_gomoku_g37.ui.theme.LightBlue
 import com.example.pdm2324i_gomoku_g37.ui.theme.Pdm2324i_gomoku_g37Theme
+import com.example.pdm2324i_gomoku_g37.ui.theme.SoftRed
 
 @Composable
 fun AuthorsScreen() {
     Pdm2324i_gomoku_g37Theme {
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
             //Greeting("Group 37")
-            AuthorsDisplay()
+            //AuthorsDisplay()
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(6.dp),
+            ) {
+                Text(
+                    text = "Group 37",
+                    fontSize = 35.sp,
+                    modifier = Modifier.
+                    padding(bottom = 15.dp)
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(4.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = SoftRed,
+                            contentColor = Color.White
+                        ),
+                        contentPadding = PaddingValues(15.dp),
+                        modifier = Modifier
+                    ) {
+                        Text(
+                            text = "Prev",
+                            fontSize = 10.sp,
+                        )
+                    }
+
+                    ElevatedCard(
+                        colors = CardDefaults.cardColors(
+                            containerColor = LightBlue,
+                        ),
+                        shape = RoundedCornerShape(5.dp),
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 4.dp
+                        ),
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .size(width = 200.dp, height = 300.dp)
+                    ) {
+                        AuthorsDisplay()
+                    }
+
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(4.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = SoftRed,
+                            contentColor = Color.White
+                        ),
+                        contentPadding = PaddingValues(15.dp),
+                        modifier = Modifier
+                    ) {
+                        Text(
+                            text = "Next",
+                            fontSize = 10.sp,
+                        )
+                    }
+                }
+            }
         }
     }
 }
--- pdm2324i_gomoku_g37/app/src/main/java/com/example/pdm2324i_gomoku_g37/ui/theme/Color.kt
+++ pdm2324i_gomoku_g37/app/src/main/java/com/example/pdm2324i_gomoku_g37/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightBlue = Color(0xFFa8dadc)
+val SoftRed = Color(0xFFe63946)
